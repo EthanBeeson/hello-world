ddf4035c873593097267d45d4d27dc670165a882
EthanBeeson <ethanmbeeson@gmail.com>
2023-10-25 10:26:20 -0400

    Revert "Added another line of code"
    This reverts commit 05d68a0937f8294133b95c63aad0b62c0ce23467.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld
 {
    public static void main(String[] args) {
-      System.out.println("Adding a second line of code");
-      System.out.println("Hello World");
+      System.out.print("Hello World");
    }
 }
